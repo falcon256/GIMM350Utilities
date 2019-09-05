95dfca4a9c9da57ae18942092c3c4319401ed5d1
Falcon256 <256games@gmail.com>
2019-09-05 10:09:56 -0600

    Did the first part of day 1

---
+++ Utility/Assets/Scripts/Futilities.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Globalization;
+using System;
+
+
+public static class Futilities
+{
+    public static string reverse(string input)
+    {
+        string output = "";
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            output += input[i];
+        }
+        return output;
+    }
+    public static string CheckIfNumOutputString(string input)
+    {
+        IFormatProvider provider = CultureInfo.CreateSpecificCulture("en-US");
+        float waste = 0;
+        if (float.TryParse(input, NumberStyles.Any, provider, out waste))
+        {
+            return "Number";
+        }
+        return "Text";
+    }
+    public static string CountfNumOutputString(string input)
+    {
+        IFormatProvider provider = CultureInfo.CreateSpecificCulture("en-US");
+        string[] strings = input.Split();
+        int numStr = 0;
+        int numNum = 0;
+        float waste = 0;
+        for (int i = 0; i < strings.Length; i++)
+        {
+            if (float.TryParse(strings[i], NumberStyles.Any, provider, out waste))
+            {
+                numNum++;
+            }
+            else
+            {
+                numStr++;
+            }
+        }
+        return "" + numNum + " Numbers and " + numStr + " Strings.";
+    }
+    }
---
+++ Utility/Assets/Scripts/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputField inputField = null;
+    public Text outputField = null;
+    void Start()
+    {
+        inputField.text = "";
+        outputField.text = "";
+    }
+
+
+    public void doUpdate()
+    {
+        string derp = inputField.text;
+        //outputField.text = Futilities.reverse(derp);
+        //outputField.text = Futilities.CheckIfNumOutputString(derp);
+        outputField.text = Futilities.CountfNumOutputString(derp);
+    }
+}
