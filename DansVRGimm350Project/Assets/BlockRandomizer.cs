c0e5b2152f55892b1b8c3f51103b3d9541ee54e4
Falcon256 <256games@gmail.com>
2019-09-19 10:30:54 -0600

    VR Stuff

---
+++ DansVRGimm350Project/Assets/BlockRandomizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockRandomizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<MeshRenderer>().material.color = new Color32((byte)Random.Range(0, 255), (byte)Random.Range(0, 255), (byte)Random.Range(0, 255), (byte)Random.Range(0, 255));
+        this.transform.localScale = Random.insideUnitSphere;
+        this.transform.localPosition = new Vector3(0, Random.Range(1, 200), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
