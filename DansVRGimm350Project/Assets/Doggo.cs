c10a63e6efe8850945276de01156a04cc9f57d8d
256 Games <256games@gmail.com>
2019-10-24 11:31:01 -0600

    More doggo network

--- DansVRGimm350Project/Assets/Doggo.cs
+++ DansVRGimm350Project/Assets/Doggo.cs
@@ -32,22 +32,43 @@ public class Doggo : MonoBehaviour
         ConfigurableJoint[] joints = this.GetComponentsInChildren<ConfigurableJoint>();
         Rigidbody[] rbs = this.GetComponentsInChildren<Rigidbody>();
         int index = 0;
-        for(int i = 0; i < rbs.Length; i++)
+        myInputs[index++] = transform.position.x;
+        myInputs[index++] = transform.position.y;
+        myInputs[index++] = transform.position.z;
+        myInputs[index++] = transform.forward.x;
+        myInputs[index++] = transform.forward.y;
+        myInputs[index++] = transform.forward.z;
+        for (int i = 0; i < rbs.Length; i++)
         {
             myInputs[index++] = rbs[i].transform.localEulerAngles.x;
             myInputs[index++] = rbs[i].transform.localEulerAngles.y;
             myInputs[index++] = rbs[i].transform.localEulerAngles.z;
             myInputs[index++] = rbs[i].transform.localPosition.x;
             myInputs[index++] = rbs[i].transform.localPosition.y;
-            myInputs[index++] = rbs[i].transform.localPosition.z;
-            //     myInputs[index++] = joints[i].
+            myInputs[index++] = rbs[i].transform.localPosition.z;          
         }
 
-
+        float[] outputs = myNetwork.Tick(myInputs);
+        index = 0;
+        for(int i = 0; i < joints.Length; i++)
+        {
+            joints[i].targetAngularVelocity = new Vector3(outputs[index++], outputs[index++], outputs[index++]);
+        }
 
         currentHighestScore *= 0.999f;
         //TODO also spawn new ones if we are the farthest one to whatever direction...
         if (lifeLived > maxLifetime)
+        {
+            //do the mutation
+
+            Network newNet = myNetwork.GetMutatedChild(0.01f, 0.01f, 0.1f);
+
+
+
+
+
             Destroy(this.gameObject);
+            
+        }
     }
 }
